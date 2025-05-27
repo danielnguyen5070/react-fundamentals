1cbbe8b706a8bb97a68aa6d10e3a6e2e7ca2c00c
vietnguyen1998 <38393657+vietnguyen1998@users.noreply.github.com>
2025-05-27 14:18:49 +0700

    add final example

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
-
-import Typescript01 from './exercises/05.typescript/01.problem.props'
-import Typescript02 from './exercises/05.typescript/02.problem.narrow'
-import Typescript03 from './exercises/05.typescript/03.problem.derive'
-import Typescript04 from './exercises/05.typescript/04.problem.default-props'
-import Typescript05 from './exercises/05.typescript/05.problem.function-types'
-import Typescript06 from './exercises/05.typescript/06.problem.satisfies'
-
-import Styling01 from './exercises/06.styling/01.problem.style'
-import Styling02 from './exercises/06.styling/02.problem.component'
-import Styling03 from './exercises/06.styling/03.problem.size-prop'
-
-import Forms01 from './exercises/07.forms/01.problem.form'
-import Forms02 from './exercises/07.forms/02.problem.action'
-import Forms03 from './exercises/07.forms/03.problem.types'
-import Forms04 from './exercises/07.forms/04.problem.submit'
-import Forms05 from './exercises/07.forms/05.problem.action'
-
-import Input1 from './exercises/08.inputs/01.problem.checkbox'
-import Input2 from './exercises/08.inputs/02.problem.select'
-import Input3 from './exercises/08.inputs/03.problem.radio'
-import Input4 from './exercises/08.inputs/04.problem.hidden'
-import Input5 from './exercises/08.inputs/05.problem.default-value'
-
-import Errors1 from './exercises/09.errors/01.problem.composition'
-import Errors2 from './exercises/09.errors/02.problem.show-boundary'
-import Errors3 from './exercises/09.errors/03.problem.reset'
-
-import Arrays1 from './exercises/10.arrays/01.problem.key-prop'
-import Arrays2 from './exercises/10.arrays/02.problem.focus-state'
-import Arrays3 from './exercises/10.arrays/03.problem.key-reset'
+import Typescript from './exercises/05.typescript'
+import Styling from './exercises/06.styling'
+import Forms from './exercises/07.forms'
+import Input from './exercises/08.inputs'
+import Errors from './exercises/09.errors'
+import Arrays from './exercises/10.arrays'
 
 const routes = [
-  { path: "/typescript/props", element: <Typescript01 /> },
-  { path: "/typescript/narrow", element: <Typescript02 /> },
-  { path: "/typescript/derive", element: <Typescript03 /> },
-  { path: "/typescript/default", element: <Typescript04 /> },
-  { path: "/typescript/duplication", element: <Typescript05 /> },
-  { path: "/typescript/satisfies", element: <Typescript06 /> },
-  { path: "/styling/styling", element: <Styling01 /> },
-  { path: "/styling/compenent", element: <Styling02 /> },
-  { path: "/styling/size-prop", element: <Styling03 /> },
-  { path: "/forms/form", element: <Forms01 /> },
-  { path: "/forms/form-action", element: <Forms02 /> },
-  { path: "/forms/input-types", element: <Forms03 /> },
-  { path: "/forms/submissionx", element: <Forms04 /> },
-  { path: "/forms/form-actions", element: <Forms05 /> },
-  { path: "/inputs/checkbox", element: <Input1 /> },
-  { path: "/inputs/select", element: <Input2 /> },
-  { path: "/inputs/radios", element: <Input3 /> },
-  { path: "/inputs/hidden-inputs", element: <Input4 /> },
-  { path: "/inputs/default-value", element: <Input5 /> },
-  { path: "/errors/composition", element: <Errors1 /> },
-  { path: "/errors/other-errors", element: <Errors2 /> },
-  { path: "/errors/reset", element: <Errors3 /> },
-  { path: "/arrays/key-prop", element: <Arrays1 /> },
-  { path: "/arrays/focus-state", element: <Arrays2 /> },
-  { path: "/arrays/key-reset", element: <Arrays3 /> },
+  { path: "/typescript", element: <Typescript /> },
+  { path: "/styling", element: <Styling /> },
+  { path: "/forms", element: <Forms /> },
+  { path: "/inputs", element: <Input /> },
+  { path: "/errors", element: <Errors /> },
+  { path: "/arrays", element: <Arrays /> },
 ];
 
 function App() {
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,71 +7,33 @@ import { useState } from 'react';
 const menuItems = [
     {
         label: 'TypeScript',
-        submenu: [
-            { label: '01. Props', path: '/typescript/props' },
-            { label: '02. Narrow Types', path: '/typescript/narrow' },
-            { label: '03. Derive Types', path: '/typescript/derive' },
-            { label: '04. Default Props', path: '/typescript/default' },
-            { label: '05. Reduce Duplication', path: '/typescript/duplication' },
-            { label: '06. Satisfies', path: '/typescript/satisfies' },
-        ],
+        path: '/typescript',
     },
     {
         label: 'Styling',
-        submenu: [
-            { label: '01. Styling', path: '/styling/styling' },
-            { label: '02. Custom Component', path: '/styling/compenent' },
-            { label: '03. Size Prop', path: '/styling/size-prop' },
-        ],
+        path: '/styling'
     },
     {
         label: 'Forms',
-        submenu: [
-            { label: '01. Form', path: '/forms/form' },
-            { label: '02. Form Action', path: '/forms/form-action' },
-            { label: '03. Input Types', path: '/forms/input-types' },
-            { label: '04. Submission', path: '/forms/submission' },
-            { label: '05. Form Actions', path: '/forms/form-actions' },
-        ],
+        path: '/forms'
     },
     {
         label: 'Inputs',
-        submenu: [
-            { label: '01. Checkbox', path: '/inputs/checkbox' },
-            { label: '02. Select', path: '/inputs/select' },
-            { label: '03. Radios', path: '/inputs/radios' },
-            { label: '04. Hidden Inputs', path: '/inputs/hidden-inputs' },
-            { label: '05. Default Value', path: '/inputs/default-value' },
-        ],
+        path: '/inputs'
     },
     {
         label: 'Error Boundaries',
-        submenu: [
-            { label: '01. Composition', path: '/errors/composition' },
-            { label: '02. Other Errors', path: '/errors/other-errors' },
-            { label: '03. Reset', path: '/errors/reset' },
-        ],
+        path: '/errors'
     },
     {
         label: 'Rendering Arrays',
-        submenu: [
-            { label: '01. Key prop', path: '/arrays/key-prop' },
-            { label: '02. Focus State', path: '/arrays/focus-state' },
-            { label: '03. Key Reset', path: '/arrays/key-reset' },
-        ],
+        path: '/arrays'
     },
 ];
 
 const Sidebar = () => {
-    const [openSubmenu, setOpenSubmenu] = useState<string | null>(null);
-
-    const toggleSubmenu = (name: string) => {
-        setOpenSubmenu((prev) => (prev === name ? null : name));
-    };
-
     return (
         <>
-
             <div
                 className={`fixed top-0 left-0 h-screen w-64 bg-gray-900 text-white z-40 transform transition-transform duration-300 ease-in-out`}
             >
@@ -79,36 +41,18 @@ const Sidebar = () => {
                 <nav className="p-4 space-y-2">
                     {menuItems.map((menu, index) => (
                         <div key={index}>
-                            <button
-                                onClick={() => toggleSubmenu(menu.label)}
-                                className={`flex my-1 items-center justify-between w-full p-2 rounded-r transition-all duration-300 ease-in-out ${openSubmenu === menu.label ? 'border-l-4 border-blue-500 bg-gray-800' : 'border-l-2 border-transparent hover:border-l-4 hover:border-blue-500 hover:bg-gray-800'
-                                    }`}
+                            <NavLink
+                                to={menu.path}
+                                className={({ isActive }) =>
+                                    `block my-1 p-2 rounded-r border-l-4 transition-all duration-300 ease-in-out ${isActive
+                                        ? 'border-blue-500 bg-gray-800 text-blue-200'
+                                        : 'border-transparent hover:bg-gray-800 hover:border-blue-500'
+                                    }`
+                                }
+                                end
                             >
-                                <span className="flex items-center space-x-3">
-                                    <span>{menu.label}</span>
-                                </span>
-                                {openSubmenu === menu.label ? <ChevronUp /> : <ChevronDown />}
-                            </button>
-                            <div
-                                className={`pl-6 space-y-2 overflow-hidden transition-all duration-300 ease-in-out ${openSubmenu === menu.label ? '' : 'max-h-0'
-                                    }`}
-                            >
-                                {menu.submenu.map((item, subIndex) => (
-                                    <NavLink
-                                        key={subIndex}
-                                        to={item.path}
-                                        className={({ isActive }) =>
-                                            `block p-2 my-1 pl-4 rounded-r border-l-4 transition-all duration-300 ease-in-out ${isActive
-                                                ? 'border-blue-500 bg-gray-800 text-blue-200'
-                                                : 'border-transparent hover:bg-gray-800 hover:border-blue-500'
-                                            }`
-                                        }
-                                        end // Ensures exact matching for active state
-                                    >
-                                        {item.label}
-                                    </NavLink>
-                                ))}
-                            </div>
+                                {menu.label}
+                            </NavLink>
                         </div>
                     ))}
                 </nav>
