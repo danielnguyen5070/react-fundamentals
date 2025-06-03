d56f6ce5e812997898384eb97edc92ce28be293b
Daniel <viet.dan.1613@gmail.com>
2025-06-03 13:41:16 +0700

    rf - style

--- src/exercises/06.styling/index.tsx
+++ src/exercises/06.styling/index.tsx
@@ -1,47 +1,18 @@
 import React from "react"
+import "./index.css"
 
-export function Box({
-	size,
-	...otherProps
-}: {
-	size?: "small" | "medium" | "large"
-} & React.ComponentProps<"div">) {
-	const sizeClasses = {
-		small: "h-20 w-20 text-sm",
-		medium: "h-28 w-28 text-base",
-		large: "h-36 w-36 text-lg",
-	}
-
-	const sizeClassName = size ? sizeClasses[size] : ""
-
-	return (
-		<div
-			{...otherProps}
-			className={[
-				"italic rounded-md shadow-md p-4 text-gray-800 text-center",
-				otherProps.className,
-				sizeClassName,
-			]
-				.filter(Boolean)
-				.join(" ")}
-			style={{ ...otherProps.style }}
-		/>
-	)
-}
+const smallBox = <div>small lightblue box</div>
+const mediumBox = <div>medium pink box</div>
+const largeBox = <div>large orange box</div>
+const sizelessColorlessBox = <div>sizeless colorless box</div>
 
 function App() {
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-100 p-6">
-			<Box size="small" style={{ backgroundColor: "lightblue" }}>
-				small lightblue box
-			</Box>
-			<Box size="medium" style={{ backgroundColor: "pink" }}>
-				medium pink box
-			</Box>
-			<Box size="large" style={{ backgroundColor: "orange" }}>
-				large orange box
-			</Box>
-			<Box>sizeless colorless box</Box>
+			{smallBox}
+			{mediumBox}
+			{largeBox}
+			{sizelessColorlessBox}
 		</div>
 	)
 }
